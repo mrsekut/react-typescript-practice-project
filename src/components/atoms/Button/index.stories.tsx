5f0a6b835cd7604d30d3f5d44582a78ae3383bb4
mrsekut <k.cloudspider@gmail.com>
2018-08-14 18:11:58 +0900

    [Add]addon actions,notes,storyshots

--- src/components/atoms/Button/index.stories.tsx
+++ src/components/atoms/Button/index.stories.tsx
@@ -1,5 +1,12 @@
 import * as React from "react";
 import { storiesOf } from "@storybook/react";
+import { withNotes } from "@storybook/addon-notes";
+import { action } from "@storybook/addon-actions";
 import Button from "./index";
 
-storiesOf("Button", module).add("Button", () => <Button>hello</Button>);
+storiesOf("Button", module).add(
+  "Button",
+  withNotes(`こんな感じでコンポーネントに備考を追加できます`)(() => (
+    <Button onClick={action("クリック!")}>hello</Button>
+  ))
+);
