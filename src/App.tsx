1c9b30f84d98babb013a930248b5ffdca3c77eab
mrsekut <k.cloudspider@gmail.com>
2019-01-03 22:25:33 +0900

    [Add]grid-layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,26 @@
 import * as React from 'react';
 import styled from 'styled-components';
-import RxjsComponent from './components/others/rxjs';
-import ContextComponent from './components/others/Context/Context';
-import DucksContainer from './components/others/DucksContainer';
+// import RxjsComponent from './components/others/rxjs';
+// import DucksContainer from './components/others/DucksContainer';
+// import ContextComponent from './components/others/Context/Context';
+// import PracticeHooks from './components/others/PracticeHooks';
+import GridLayout from './components/others/GridLayout';
 
 const App = () => (
   <Wrapper>
     <Header>
       <Title>React and TypeScript Test Practice</Title>
     </Header>
-    <RxjsComponent />
-    <DucksContainer />
-    <ContextComponent />
+    {/* <RxjsComponent /> */}
+    {/* <DucksContainer /> */}
+    <GridLayout />
+    {/* <PracticeHooks
+      text="Focused, hard work is the real key
+      to success. Keep your eyes on the goal,
+      and just keep taking the next step
+      towards completing it.`"
+      maxLength={5}
+    /> */}
   </Wrapper>
 );
 
